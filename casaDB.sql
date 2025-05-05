5db5f9d089249df534717b8294f5e398358c61cf
Isaac <lorenzociani1002@gmail.com>
2025-05-05 16:04:51 +0200

    db casa corretto

--- casaDB.sql
+++ casaDB.sql
@@ -1,97 +1,99 @@
-CREATE TABLE ambienti -- ambienti 1:n dispositivi // ambienti 1:n permessi
+CREATE TABLE ambiente
 (
-    id_ambienti VARCHAR(5) NOT NULL AUTO_INCREMENT,
+    id_ambiente INT NOT NULL AUTO_INCREMENT,
     interno BOOLEAN,
     nome VARCHAR(15),
     superficie FLOAT,
     altezza FLOAT,
     immagine VARCHAR(255),
-    PRIMARY KEY (id_ambienti)
+    PRIMARY KEY (id_ambiente)
 );
 
-CREATE TABLE tipologia --tipologia n:m dispositivi
+CREATE TABLE tipologia
 (
-    id_tipologia VARCHAR(5) NOT NULL AUTO_INCREMENT,
+    id_tipologia INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(15),
     attuatore BOOLEAN,
     PRIMARY KEY (id_tipologia)
 );
 
-CREATE TABLE dispositivi --dispositivi 1:n accessi // dispositivi 1:n letture // dispositivi n:1 ambienti
+CREATE TABLE dispositivo
 (
-    id_dispositivi VARCHAR(5) NOT NULL AUTO_INCREMENT,
+    id_dispositivo INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(25),
     descrizione TEXT,
     data_installazione DATE,
     data_ultima_manutenzione DATE,
-    timestamp_ultima_lettura TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    id_ambiente VARCHAR(5),
-    PRIMARY KEY (id_dispositivi),
+    timestamp_ultima_lettura TIMESTAMP,
+    id_ambiente INT,
+    PRIMARY KEY (id_dispositivo),
     FOREIGN KEY (id_ambiente) REFERENCES ambiente (id_ambiente)
 );
 
 
-CREATE TABLE tipologia_dispositivi --tipologia n:m dispositivi
+CREATE TABLE tipologia_dispositivi
 (
-    id VARCHAR(5) NOT NULL AUTO_INCREMENT,
-    id_tipologia VARCHAR(5),
-    id_dispositivi VARCHAR(5),
-    id_tipologia VARCHAR(5),
+    id INT NOT NULL AUTO_INCREMENT,
+    id_tipologia INT,
+    id_dispositivo INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (id_dispositivi) REFERENCES dispositivi (id_dispositivi),
+    FOREIGN KEY (id_dispositivo) REFERENCES dispositivo (id_dispositivo),
     FOREIGN KEY (id_tipologia) REFERENCES tipologia (id_tipologia)
 );
 
-CREATE TABLE letture --letture n:1 dispositivi
+CREATE TABLE lettura
 (
-    id_letture VARCHAR(5) NOT NULL AUTO_INCREMENT,
-    id_dispositivi VARCHAR(5),
-    valore_numerico FLOAT(24),
-    valore_test FLOAT(24),
-    valore_booleano BOOLEAN(24),
+    id_lettura INT NOT NULL AUTO_INCREMENT,
+    id_dispositivo INT,
+    valore_numerico FLOAT(3,2),
+    valore_test FLOAT(3,2),
+    valore_booleano BOOLEAN,
 
-    PRIMARY KEY (id_letture),
-    FOREIGN KEY (id_dispositivi) REFERENCES dispositivi (id_dispositivi)
+    PRIMARY KEY (id_lettura),
+    FOREIGN KEY (id_dispositivo) REFERENCES dispositivo (id_dispositivo)
 );
 
-CREATE TABLE permessi --permessi n:m utente // permessi n:1 ambienti
+CREATE TABLE permesso
 (
-    id_permessi VARCHAR(5) NOT NULL AUTO_INCREMENT,
+    id_permesso INT NOT NULL AUTO_INCREMENT,
+    id_ambiente INT,
     nome VARCHAR(25),
     descrizione TEXT,
-    PRIMARY KEY (id_permessi)
-);
-
-CREATE TABLE utenti --utente n:m permessi // utente n:1 ambienti
-(
-    id_utenti VARCHAR(5) NOT NULL AUTO_INCREMENT,
-    nome varchar(25),
-    cognome varchar(25),
-    impronta_biometrica VARCHAR(255), --da capire che tipo di variabile usare
-    user_password VARCHAR(30),
-    data_nascita DATE,
-    id_ambiente VARCHAR(5),
-    id_permessi VARCHAR(5),
-    PRIMARY KEY (id_utenti),
+    PRIMARY KEY (id_permesso),
     FOREIGN KEY (id_ambiente) REFERENCES ambiente (id_ambiente)
 );
 
-CREATE TABLE utente_permessi --utente n:m permessi
+CREATE TABLE utente
 (
-    id VARCHAR(5) NOT NULL AUTO_INCREMENT,
-    id_utenti VARCHAR(5),
-    id_permessi VARCHAR(5),
+    id_utente INT NOT NULL AUTO_INCREMENT,
+    nome varchar(25),
+    cognome varchar(25),
+    impronta_biometrica VARCHAR(255),
+    user_password VARCHAR(32),
+    data_nascita DATE,
+    id_permesso INT,
+    PRIMARY KEY (id_utente),
+    FOREIGN KEY (id_permesso) REFERENCES permesso (id_permesso)
+);
+
+CREATE TABLE utente_permesso
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    id_utente INT,
+    id_permesso INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (id_utenti) REFERENCES utenti (id_utenti),
-    FOREIGN KEY (id_permessi) REFERENCES permessi (id_permessi)
+    FOREIGN KEY (id_utente) REFERENCES utente (id_utente),
+    FOREIGN KEY (id_permesso) REFERENCES permesso (id_permesso)
 );
 
 
-CREATE TABLE accessi --accessi n:1 dispositivi // accessi n:1 utente
+CREATE TABLE accesso
 (
-    id_accessi VARCHAR(5) NOT NULL AUTO_INCREMENT,
+    id_accesso INT NOT NULL AUTO_INCREMENT,
     timestamp_accesso TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id_accessi),
-    FOREIGN KEY (id_utenti) REFERENCES utenti (id_utenti),
-    FOREIGN KEY (id_dispositivi) REFERENCES dispositivi (id_dispositivi)
+    id_utente INT,
+    id_dispositivo INT,
+    PRIMARY KEY (id_accesso),
+    FOREIGN KEY (id_utente) REFERENCES utente (id_utente),
+    FOREIGN KEY (id_dispositivo) REFERENCES dispositivo (id_dispositivo)
 );
